088133d5e9b3c0ea8542563ae671e63a1e5ab274
Dean <deangnjidic@gmail.com>
2023-08-27 18:55:26 -0500

    start

---
+++ main.js
@@ -0,0 +1,9 @@
+function playRound(playerSelection, computerSelection) {
+    let = Rock
+    let = Scissors
+    let = Papper
+  }
+   
+  const playerSelection = "rock";
+  const computerSelection = getComputerChoice();
+  console.log(playRound(playerSelection, computerSelection));
