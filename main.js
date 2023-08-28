6fe16b662cc9a57bae793d5820d1940fb7d43291
Dean <deangnjidic@gmail.com>
2023-08-27 19:18:58 -0500

    1 step

--- main.js
+++ main.js
@@ -1,9 +1,28 @@
+function getComputerChoice() {
+  const choices = ['Rock', 'Paper', 'Scissors'];
+  const randomIndex = Math.floor(Math.random() * choices.length);
+  return choices[randomIndex];
+}
+
 function playRound(playerSelection, computerSelection) {
-    let = Rock
-    let = Scissors
-    let = Papper
+  playerSelection = playerSelection.toLowerCase();
+  computerSelection = computerSelection.toLowerCase();
+
+  if (playerSelection === computerSelection) {
+    return "It's a tie!";
+  } else if (
+    (playerSelection === 'rock' && computerSelection === 'scissors') ||
+    (playerSelection === 'paper' && computerSelection === 'rock') ||
+    (playerSelection === 'scissors' && computerSelection === 'paper')
+  ) {
+    return `You Win! ${playerSelection} beats ${computerSelection}`;
+  } else {
+    return `You Lose! ${computerSelection} beats ${playerSelection}`;
   }
-   
-  const playerSelection = "rock";
-  const computerSelection = getComputerChoice();
-  console.log(playRound(playerSelection, computerSelection));
+}
+
+// Example usage
+const playerSelection = 'Rock'; // You can replace this with user input
+const computerSelection = getComputerChoice();
+const result = playRound(playerSelection, computerSelection);
+console.log(result);
